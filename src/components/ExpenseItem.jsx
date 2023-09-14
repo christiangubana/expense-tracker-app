2f468ae36a140b2925c3136b6c9861f49d1f130f
christiangubana <christiangubana@gmail.com>
2023-09-14 19:12:05 +0200

    initial commit

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+const ExpenseItem = ({date, title, amount}) => {
+  return (
+    <div className='expense-item'>
+        <div>{date}</div>
+        <div className='expense-item__description'>
+            <h2>{title}</h2>
+            <div className='expense-item__price'>R{amount}</div>
+        </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
